8701b57a84b830a8c1763c12ef48aa87421ef37e
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-08-02 19:03:47 +0900

    commit

--- src/main/java/FileScheduleWatcher.java
+++ src/main/java/FileScheduleWatcher.java
@@ -1,10 +1,52 @@
 import java.io.IOException;
-import java.nio.file.CopyOption;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
-public class FileScheduleWatcher {
+public class FileScheduleWatcher extends FileWatcher {
+    public FileScheduleWatcher(Path targetFolder, Path resultFolder) {
+        super(targetFolder, resultFolder);
+    }
 
+    public static void main(String[] args) {
+        //args 사용
 
+    }
+
+    @Override
+    public void startWatch() {
+        try {
+            Files.walkFileTree(targetFolder, new FileVisitor<Path>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                    Path resultDir = Paths.get(resultFolder.toString() + dir.toString().replace(targetFolder.toString(), ""));
+                    if (!Files.exists(resultDir)) {
+                        Files.createDirectories(resultDir);
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    if ((!file.toString().contains(".part")) && (!file.toString().contains(".encrypted"))) {
+                        Path resultFile = Paths.get(resultFolder.toString() + file.toString().replace(targetFolder.toString(), ""));
+                        Files.move(file, resultFile, copyOptions);
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException e) {
+            System.out.println("폴더 순회중 오류가 발생했습니다.");
+        }
+    }
 
 }
--- src/main/java/FileWatcher.java
+++ src/main/java/FileWatcher.java
@@ -1,38 +1,21 @@
 import java.io.IOException;
-import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.List;
+import java.nio.file.CopyOption;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 public abstract class FileWatcher {
     protected Path targetFolder;
     protected Path resultFolder;
     protected CopyOption[] copyOptions;
 
-    public FileWatcher(Path targetFolder,Path resultFolder){
+    public FileWatcher(Path targetFolder, Path resultFolder) {
         copyOptions = new CopyOption[]{StandardCopyOption.REPLACE_EXISTING};
         try {
             this.targetFolder = targetFolder.toRealPath();
             this.resultFolder = resultFolder.toRealPath();
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             this.targetFolder = null;
             this.resultFolder = null;
-        }
-    }
-
-    final public void moveFile(Path fileName){
-        Path filePath = Paths.get(this.targetFolder.toString(),fileName.toString());
-        Path resultFilePath = Paths.get(this.resultFolder.toString(),fileName.toString());
-
-        try {
-            BasicFileAttributes basicFileAttributes = Files.readAttributes(filePath, BasicFileAttributes.class);
-            if((!fileName.toString().contains(".part"))
-                    && (!fileName.toString().contains(".encrypted"))
-                    && (!basicFileAttributes.isDirectory())){
-                Files.move(filePath,resultFilePath,copyOptions);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 
